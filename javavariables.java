f6ac79e507fd4a05fe0dd6cbb1f89a77fd42d388
David Anderson <david.anderson1993@gmail.com>
2022-04-15 14:46:33 -0500

    adding java practice file

---
+++ javavariables.java
@@ -0,0 +1,62 @@
+package main;
+
+public class javavariables {
+
+	public static void main(String[] args) {
+		//1.
+		int num1 = 8;
+		int num2 = 9;
+		int sum = num1 + num2;
+		System.out.println(sum);
+		
+		//2.
+		double num3 = 23.23;
+		double num4 = 30.30;
+		double sum2 = num3 + num4;
+		System.out.println(sum2);
+		
+		//3.
+		int num5 = 76;
+		double num6 =76.76;
+		double sum3 = num5+num6;
+		System.out.println(sum3);
+		
+		//4.
+		int num7 = 6;
+		int num8 = 8;
+		double sum4 = num8 / num7;
+		System.out.println(sum4);
+		
+		//5.
+		double num9 = 6.6;
+		double num10 = 8.8;
+		double sum5 = num10 / num9;
+		System.out.println(sum5);
+		
+		
+		//6.
+		int x = 5;
+		int y = 6;
+		double q = y / x;
+		System.out.println(q);
+		
+		//7.
+		double number = 76;
+		double anotherNumber = 88;
+		double bigSum = number + anotherNumber;
+		System.out.println(bigSum);
+		
+		//8.
+		double coffee = 1.99;
+		double Bagel = 2.99;
+		double chocolate = 3.99;
+		
+		double order = (coffee * 3) + (Bagel * 4) + (chocolate * 2); 
+		double subTotal = order;
+		double SALES_TAX = .89;
+		double totalSale = subTotal + SALES_TAX;
+		System.out.println(totalSale);
+		
+	}
+
+}
